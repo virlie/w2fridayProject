92df4b03ffba004e5bf6065f3118756121a4786e
Joy Paglinawan <virliejoyp@yahoo.com>
2018-03-23 16:43:10 -0700

    Add questions and gave variety of answers to choose from. Add different alerts depending on what answers users give.

--- js/scripts.js
+++ js/scripts.js
@@ -18,23 +18,35 @@ function questionCheck (question) {
   }
 
   function mostCommon (j,r,p) {
-    var return: add value
+    if ( j > r && j > p) {
+      alert("you should take java!")
+    } else if ( r > j && r > p) {
+      alert("you should take ruby!")
+    } else if ( p > r && p > j) {
+      alert("you should be in php!")
+    } else if ( r == j && r > p ){
+      alert("you could take ruby or java")
+    } else if ( r == p && r > j ){
+      alert("you could take ruby or php")
+    } else if ( p == j && p > r ){
+      alert("you could take php or java")
+    } else {
+      alert("you are a very interesting person! flip a coin :)")
+    }
   }
 
   $(".quiz").submit(function() {
-
-    for (i = 1; i < 6; i++) {
+    // dynamic variables
+    for (i = 1; i < 11; i++) {
       window['question'+i] = ($(("#question"+i)+" option:selected")).val()
       questionCheck(parseInt(window['question'+i]))
     }
-
     //log current counts
     console.log("java count: " + java)
     console.log("ruby count: " + ruby)
     console.log("php count: " + php)
-
     //find most common and alert user to their track
-
+    mostCommon(java,ruby,php)
 
   })
 })
